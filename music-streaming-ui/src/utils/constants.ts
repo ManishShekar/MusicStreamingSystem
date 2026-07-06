bbe3ac6acc06db778ff49ea5a0d738b12c7dcc35
ManishShekar <95437686+ManishShekar@users.noreply.github.com>
2026-07-07 00:14:50 +0530

    Update constants.ts

--- music-streaming-ui/src/utils/constants.ts
+++ music-streaming-ui/src/utils/constants.ts
@@ -1,6 +1,6 @@
 // export const API_BASE_URL: string ="https://localhost:44388/api";
 export const API_BASE_URL: string ="http://localhost:92/api";
-// export const API_BASE_URL: string ="http://192.168.1.108/api";
+
 
 
 export const FILE_BASE_URL: string = API_BASE_URL.replace(/\/api\/?$/, "");
